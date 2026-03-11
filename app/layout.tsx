67345ff23fc7faff843f58ac28d53da27c817f8e
prajwal-mk05 <prajwalmk@tiameds.ai>
2026-03-11 10:24:04 +0530

    Favicon fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,14 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "ARC Sportzone",
   description: "Mysuru’s Premier Sports & Fitness Destination",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
